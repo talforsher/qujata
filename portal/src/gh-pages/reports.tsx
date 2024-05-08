c28d9dd80e0b7d99b396942dfdaac476a3e0c90e
Tal Forsher <tal@webz.io>
2024-05-08 09:27:07 +0300

    json pretty print

--- portal/src/gh-pages/reports.tsx
+++ portal/src/gh-pages/reports.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
+import JSONPretty from "react-json-pretty";
 interface File {
   path: string;
   name: string;
@@ -8,26 +9,30 @@ export const Reports: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<string | null>(null);
   const [fileContent, setFileContent] = useState<string>("");
   useEffect(() => {
-    const apiUrl = 'https://api.github.com/repos/att/qujata/contents/reports?ref=main';
+    const apiUrl =
+      "https://api.github.com/repos/att/qujata/contents/reports?ref=main";
     fetch(apiUrl)
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((data: File[]) => {
         setFiles(data);
-        if (data[0]) { // Check if there are any files
+        if (data[0]) {
+          // Check if there are any files
           setSelectedFile(data[0].path); // Set the first file as the default selected file
         }
       })
-      .catch(error => console.error("Error fetching files: ", error));
+      .catch((error) => console.error("Error fetching files: ", error));
   }, []);
   useEffect(() => {
     if (selectedFile) {
-    fetch(`https://raw.githubusercontent.com/att/qujata/main/${selectedFile}`)
-        .then(response => response.json())
+      fetch(`https://raw.githubusercontent.com/att/qujata/main/${selectedFile}`)
+        .then((response) => response.json())
         .then((data: any) => {
           const fileContent = JSON.stringify(data, null, 2); // Decode the base64 content
           setFileContent(fileContent);
         })
-        .catch(error => console.error("Error fetching file content: ", error));
+        .catch((error) =>
+          console.error("Error fetching file content: ", error)
+        );
     }
   }, [selectedFile]); // This useEffect hook is dependent on selectedFile
   const handleFileChange = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -35,14 +40,19 @@ export const Reports: React.FC = () => {
     setSelectedFile(value);
   };
   return (
-    <div><select value={selectedFile || ''} onChange={handleFileChange}>
-    {files.map((file, index) => (
-      <option key={index} value={file.path}>
-        {file.name}
-      </option>
-    ))}
-  </select><br/><br/><pre>
-    {fileContent}
-  </pre></div>
+    <div>
+      <select value={selectedFile || ""} onChange={handleFileChange}>
+        {files.map((file, index) => (
+          <option key={index} value={file.path}>
+            {file.name}
+          </option>
+        ))}
+      </select>
+      <br />
+      <br />
+      <pre>
+        <JSONPretty id="json-pretty" data={fileContent}></JSONPretty>
+      </pre>
+    </div>
   );
-}
+};
